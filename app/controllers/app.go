91a1c2e4c8815264522cea3fd59aa7133d9750da
Ruben Meza <rmezar@gmail.com>
2021-03-23 10:16:55 -0600

    feat(demo): creating an application

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -11,3 +11,15 @@ type App struct {
 func (c App) Index() revel.Result {
 	return c.Render()
 }
+
+func (c App) Order() revel.Result {
+	return c.Render()
+}
+
+func (c App) CreateAccount() revel.Result {
+	return c.RenderTemplate("App/create_account.html")
+}
+
+func (c App) Payment() revel.Result {
+	return c.Render()
+}
